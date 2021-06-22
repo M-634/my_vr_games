153426c1ab84c4995c5ca314d2cbb318660a5754
teruteru634 <miyanagateru634@gmail.com>
2021-06-22 23:43:51 +0900

    ハンドガンの処理をリファクタリング

--- Assets/_DualWield/Scripts/HundGunCotrol.cs
+++ Assets/_DualWield/Scripts/HundGunCotrol.cs
@@ -14,11 +14,19 @@ namespace Musahi.MY_VR_Games.DualWield
         [SerializeField] ActionBasedController controller;
         [SerializeField] string AnimatorShotName;
 
+        [SerializeField] Transform muzzle;
+        [SerializeField] float maxShotRange = 10f;
+
+
+        LineRenderer lineRenderer;
         Animator anim;
 
         private void Start()
         {
             anim = GetComponent<Animator>();
+            lineRenderer = GetComponent<LineRenderer>();
+
+            lineRenderer.SetPositions(new Vector3[] { muzzle.position, muzzle.position });
             controller.activateAction.action.performed += Action_TriggerPressed;
         }
 
@@ -26,17 +34,36 @@ namespace Musahi.MY_VR_Games.DualWield
         {
             if (anim)
             {
-                if (anim.GetCurrentAnimatorStateInfo(0).IsName(AnimatorShotName)) return;
+                if (anim.GetCurrentAnimatorStateInfo(0).IsName(AnimatorShotName)) return;//銃を撃つアニメーションが再生中は、トリガー押しても何も起こらない。
                 anim.Play(AnimatorShotName);
             }
         }
 
         /// <summary>
-        /// アニメーションイベントから呼ばれる
+        /// アニメーションイベントから呼ばれる。
+        /// Rayを飛ばして、敵ターゲットに当たったか調べる。当たったら、敵を倒す。
         /// </summary>
         public void Shot()
         {
-            Debug.Log("shot!!");
+            DrawLine();
+        }
+
+
+        private async void DrawLine()
+        {
+            var isHit = Physics.Raycast(muzzle.position, muzzle.forward, out RaycastHit hit, maxShotRange);
+
+            if (isHit)
+            {
+                if(hit.transform.TryGetComponent(out IDamagable target))
+                {
+                    target.OnDamage();
+                }
+                lineRenderer.SetPosition(1, hit.point);
+            }
+            lineRenderer.SetPosition(1, muzzle.position + muzzle.forward * maxShotRange);
+
+        
         }
     }
 }
